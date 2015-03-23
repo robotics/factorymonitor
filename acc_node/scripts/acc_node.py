2b2bda456924ab022974ad7dd28a5e3b325bc241
Ankit Bhatia <ankitbhatia@gmail.com>
2015-03-23 18:41:44 -0400

    Writing acc_node code. factorymonitor package.xml works now

--- acc_node/scripts/acc_node.py
+++ acc_node/scripts/acc_node.py
@@ -11,6 +11,15 @@ def main():
 
     while True:
         data = s.readline()
+        d = data.split(',')
+        ax = int(d[2])
+        ay = int(d[3])
+        az = int(d[4])
+        crc = int(d[5])
+        if ax+ay+az is not crc:
+            rospy.logerr("crc does not match up")
+        else:
+
         # rospy.loginfo(data) 
         print data
 if __name__ == '__main__':
