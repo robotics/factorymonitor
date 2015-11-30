36c9fb10392363f7f0e6c89c23d5464828824b7c
Ankit Bhatia <ankitb@cmu.edu>
2015-11-30 13:10:55 -0500

    Added publisher to change_viewer. Testing required.

--- change_viewer/src/change_viewer.cpp
+++ change_viewer/src/change_viewer.cpp
@@ -7,10 +7,12 @@
 #include <boost/foreach.hpp>
 #include <ros/console.h>
 #include <pcl/filters/extract_indices.h>
+#include "std_msgs/Int64.h"
 
 typedef pcl::PointCloud<pcl::PointXYZ> PointCloud;
 float resolution = 1.0f;
 pcl::octree::OctreePointCloudChangeDetector<pcl::PointXYZ> octree (resolution);
+ros::Publisher pub;
 
 void callback(const PointCloud::ConstPtr& msg)
 {
@@ -22,6 +24,9 @@ void callback(const PointCloud::ConstPtr& msg)
   std::vector<int> newPointIdxVector;
   octree.getPointIndicesFromNewVoxels(newPointIdxVector);
   ROS_INFO("Indices_changed = %lu", newPointIdxVector.size());
+  std_msgs::Int64 msg_publish;
+  msg_publish.data = newPointIdxVector.size();
+  pub.publish(msg_publish);
 
 }
 
@@ -30,5 +35,6 @@ int main(int argc, char** argv)
   ros::init(argc, argv, "change_viewer");
   ros::NodeHandle nh;
   ros::Subscriber sub = nh.subscribe<PointCloud>("/satellite1/sd/points", 1, callback);
+  pub = nh.advertise<std_msgs::Int64>("commotion",10);
   ros::spin();
 }
